a574c9522bc706650da0c18e40f2e89a1394c4c8
Neto test <yury120405@gmail.com>
2024-10-17 20:57:01 +0400

    Initial commit

---
+++ Unique vector/Unique vector.cpp
@@ -0,0 +1,21 @@
+﻿#include <iostream>
+#include <vector>
+#include <set>
+#include <algorithm>
+
+std::vector<int> removeDuplicates(const std::vector<int>& input) {
+	std::set<int> uniqueElements(input.begin(), input.end());
+	std::vector<int> result(uniqueElements.begin(), uniqueElements.end());
+	return result;
+}
+
+int main() {
+	std::vector<int> input = { 1, 1, 2, 5, 6, 1, 2, 4 };
+
+	std::vector<int> result = removeDuplicates(input);
+
+	for (int num : result) {
+		std::cout << num << " ";
+	}
+
+}
